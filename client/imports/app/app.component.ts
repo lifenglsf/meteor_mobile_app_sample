bc738099db1f427b7a30f0180bb8ed3e4625fd35
Tom Li <lifenglsf@163.com>
2019-07-21 17:05:53 +0800

    add ionic

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
-
+import {Platform} from 'ionic-angular';
+import {StatusBar,Splashscreen} from 'ionic-native';
 @Component({
   selector: 'app',
   templateUrl: 'app.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      if (platform.is('cordova')) {
+        StatusBar.styleDefault();
+        Splashscreen.hide();
+      }
+    });
+  }
+}
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,ErrorHandler} from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,14 +11,15 @@ import { MatDialogModule } from '@angular/material';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DialogErrorComponent} from './dialog/dialog.error.component';
 import {DialogSuccessComponent} from './dialog/dialog.success.component';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     MatDialogModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    IonicModule.forRoot(AppComponent),
   ],
   declarations: [
     AppComponent,
@@ -26,11 +27,14 @@ import {DialogSuccessComponent} from './dialog/dialog.success.component';
     DialogSuccessComponent
   ],
   bootstrap: [
-    AppComponent
+    IonicApp
   ],
   entryComponents:[
     DialogErrorComponent,
     DialogSuccessComponent
+  ],
+  providers: [
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule { }
